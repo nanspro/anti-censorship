e076ab92b6ad25dca1054251d9072acdd30290ae
nanspro <rahulbishnoi3636@gmail.com>
2020-05-17 15:43:53 +0530

    twitter stream

--- filter.js
+++ filter.js
@@ -3,7 +3,7 @@ API_KEY = 'AIzaSyAdU3V2-zmidBMLO0s3S0b2TeCy7n8AkjM'
 
 const client = new Client(API_KEY);
 
-(async () => {
+(async function filterText () {
   let results = await client.getScores("Fuck you bitch!!", {attributes: ["TOXICITY"]});
   console.log("result", results);
   })().catch(e => {
