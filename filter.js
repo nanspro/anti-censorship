0cd9ecb38882c2e43ab98011d9bd5d2f205fa49a
Sounak Pradhan <sounak.98@gmail.com>
2020-05-17 22:15:03 +0530

    feat: update tumblr script

--- filter.js
+++ filter.js
@@ -1,11 +1,50 @@
 const { Client } = require("@conversationai/perspectiveapi-js-client");
-API_KEY = 'AIzaSyAdU3V2-zmidBMLO0s3S0b2TeCy7n8AkjM'
+API_KEY = "AIzaSyAdU3V2-zmidBMLO0s3S0b2TeCy7n8AkjM";
 
 const client = new Client(API_KEY);
 
-(async function filterText () {
-  let results = await client.getScores("Fuck you bitch!!", {attributes: ["TOXICITY"]});
-  console.log("result", results);
-  })().catch(e => {
-  console.log(e.error);
-});
+const attributes = [
+  "TOXICITY",
+  "SEVERE_TOXICITY",
+  "IDENTITY_ATTACK",
+  "INSULT",
+  "PROFANITY",
+  "THREAT",
+  "SEXUALLY_EXPLICIT",
+  "FLIRTATION",
+];
+const threshold = 0.2;
+
+// example sentences
+const examples = [
+  "fuck you bitch!",
+  "I am just being very polite.",
+  "I am being rude.",
+  // this gets marked as false which should not be the case.
+  "A notable member of the parliament has 15 rape allegations against him.",
+];
+
+async function getAverageScore(textToFilter) {
+  const scores = await getScores(textToFilter);
+  const scoresList = Object.values(scores);
+  let average = 0.0;
+
+  scoresList.forEach(function (score) {
+    average += score;
+  });
+  average /= scoresList.length;
+
+  return average;
+}
+
+async function getScores(textToFilter) {
+  let scores = {};
+  try {
+    scores = await client.getScores(textToFilter, { attributes });
+  } catch (e) {
+    console.error(`Failed with error: ${e}`);
+  }
+  return scores;
+}
+
+module.exports = { threshold, getAverageScore, getScores };
--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-const koa = require('koa')
-const Router = require('koa-router')
-const locale = require('koa-locale');
-const logger = require('koa-logger');
-const cors = require('@koa/cors');
+const koa = require("koa");
+const Router = require("koa-router");
+const locale = require("koa-locale");
+const logger = require("koa-logger");
+const cors = require("@koa/cors");
 
-const app = new koa()
-const router = new Router()
+const app = new koa();
+const router = new Router();
 
-router.get('/', (ctx) => {
-  ctx.body = "Welcome! To the Koala Book of Everything!"
-})
+router.get("/", (ctx) => {
+  ctx.body = "Welcome! To the Koala Book of Everything!";
+});
 
 locale(app);
 
 // CORS
-app.use(cors({
-	'Access-Control-Allow-Origin': '*',
-	'Access-Control-Allow-Headers': 'Origin, Accept-Language, Authorization, X-Requested-With, Content-Type, Accept',
-	'Access-Control-Allow-Methods': 'PUT, POST, GET, DELETE, PATCH, OPTIONS',
-}));
+app.use(
+  cors({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers":
+      "Origin, Accept-Language, Authorization, X-Requested-With, Content-Type, Accept",
+    "Access-Control-Allow-Methods": "PUT, POST, GET, DELETE, PATCH, OPTIONS",
+  })
+);
 
 app.use(logger());
-app.use(router.routes()).use(router.allowedMethods())
+app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(1234, () => console.log('running on port 1234'))
+app.listen(1234, () => console.log("running on port 1234"));
