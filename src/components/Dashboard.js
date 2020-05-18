9d4d526d09d0421b3ce8b6b757d4a2200257c849
Sounak Pradhan <sounak.98@gmail.com>
2020-05-18 10:29:36 +0530

    chore: update structure

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Heading } from "grommet";
+
+const Dashboard = () => {
+  return (
+    <Box>
+      <Heading>Hello World</Heading>
+    </Box>
+  );
+};
+
+export default Dashboard;
